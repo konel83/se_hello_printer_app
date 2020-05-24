8f55cf84c85a5499bc72657f8874e383d6a97671
konel83 <konel83@users.noreply.github.com>
2020-05-24 14:26:32 +0200

    Added tests to name param.

--- test/test_views.py
+++ test/test_views.py
@@ -1,8 +1,10 @@
 import unittest
+from ddt import ddt, data
 from hello_world import app
 from hello_world.formater import SUPPORTED
 
 
+@ddt
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
@@ -13,14 +15,21 @@ class FlaskrTestCase(unittest.TestCase):
         s = str(rv.data)
         ','.join(SUPPORTED) in s
 
-    def test_msg_with_output_1(self):
-        rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Kondziu", "mgs":"Hello World!"}', rv.data)  # noqa
+    @data('Ania', 'Ola', 'Konrad')
+    def test_msg_with_output_1(self, name):
+        query = 'name=' + name + '&output=json'
+        rv = self.app.get('/?' + query)
+        correct_res = b'{ "imie":"'\
+                      + bytes(name.encode('utf-8'))\
+                      + b'", "mgs":"Hello World!"}'
+        self.assertEqual(correct_res, rv.data)  # noqa
 
-    def test_msg_with_output_2(self):
-        rv = self.app.get('/?output=xml')
+    @data('Basia', 'Wojtek', 'Tester')
+    def test_msg_with_output_2(self, name):
+        query = 'name=' + name + '&output=xml'
+        rv = self.app.get('/?' + query)
         correct_output = ('<greetings>\n\t<name>'
-                          + 'Kondziu'
+                          + name
                           + '</name>\n\t<msg>'
                           + 'Hello World!'
                           + '</msg>\n</greetings>\n')
